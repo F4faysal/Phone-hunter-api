e92721f97ec5dcc91f345cd77548b062baab1199
faysal hossain <faysal15-14743@diu.edu.bdy 			+extext??!.EXIT>
2022-09-06 02:20:14 +0600

    add a show all button

--- app.js
+++ app.js
@@ -7,30 +7,37 @@
 
 // lodePhone();
 
-const lodePhone = async () => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+console.log('hello')
+const lodePhone = async (searchText , dataLemit) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(url);
   const data = await res.json();
-  displayPhone(data.data);
+  displayPhone(data.data , dataLemit);
   console.log(data.data)
 };
-const displayPhone = (phones) => {
+const displayPhone = (phones , dataLemit) => {
   const phoneContner = document.getElementById("display-phone");
   phoneContner.textContent = "";
   //   only 18 phone shoiw
-  phones = phones.slice(0, 18);
 
-
-  // //phone scearch not fund
-  // const nophone = document.getElementById("no-phone");
-  // if (phones.length === 0) {
-  //   nophone.classList.remove("d-none");
-  // }
-  // //all phone
-  // else {
-  //   nophone.classList.add("d-none");
-  // }
-
+  const morButton  = document.getElementById("mor-buttom");
+  if (phones.length > dataLemit){
+    phones = phones.slice(0, dataLemit);
+    morButton.classList.remove('d-none')
+  }
+  else{
+    morButton.classList.add('d-none')
+  }
+  //phone scearch not fund
+  const nophone = document.getElementById("no-phone");
+  if (phones.length === 0) {
+    nophone.classList.remove("d-none");
+  }
+  //all phone
+  else {
+    nophone.classList.add("d-none");
+  }
+//loop and cret html
   phones.forEach((phone) => {
     const phoneDiv = document.createElement("div");
     phoneDiv.classList.add("col");
@@ -49,26 +56,31 @@ const displayPhone = (phones) => {
   });
 
   //stop loder
-  // toggoleSpring(false);
+  toggoleSpring(false);
 };
 
-// const toggoleSpring = (loding) => {
-//   const loder = document.getElementByI("loder");
-//   if (loding) {
-//     loder.classList.remove("d-none");
-//   } else {
-//     loder.classList.add("d-none");
-//   }
-// };
+const toggoleSpring = (loding) => {
+ const logingSpring = document.getElementById('loding-spring')
+  if (loding) {
+    logingSpring.classList.remove("d-none");
+  } else {
+    logingSpring.classList.add("d-none");
+  }
+};
+
+const processSerch = (dataLemit)=> {
+   //start loder
+   toggoleSpring(true);
+   const searchValue = document.getElementById("phone-search-input");
+   const searchRasult = searchValue.value;
+   lodePhone(searchRasult , dataLemit);
+    //  console.log(searchRasult);
+}
 
 document.getElementById("Phone-Search-btn").addEventListener("click", () => {
-  //start loder
-  // toggoleSpring(true);
-  const searchValue = document.getElementById("phone-search-input");
-  const searchRasult = searchValue.value;
-  console.log(searchRasult);
-  
+  processSerch(18)
 });
 
-lodePhone();
-
+document.getElementById('show-all').addEventListener('click', ()=>{
+  processSerch();
+})
